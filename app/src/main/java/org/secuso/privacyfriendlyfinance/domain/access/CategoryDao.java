8ed161d6880dea0bcce16869c52bed2334edb6d0
Leo <leonard.otto@mailbox.org>
2018-11-14 14:19:59 +0100

    Modified Category and category dao.

--- app/src/main/java/org/secuso/privacyfriendlyfinance/domain/access/CategoryDao.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/domain/access/CategoryDao.java
@@ -1,4 +1,18 @@
 package org.secuso.privacyfriendlyfinance.domain.access;
 
-public class CategoryDao {
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import org.secuso.privacyfriendlyfinance.domain.model.Category;
+
+import java.util.List;
+
+@Dao
+public interface CategoryDao {
+    @Query("SELECT * FROM Category")
+    List<Category> getAll();
+
+    @Insert
+    void insert(Category category);
 }
--- app/src/main/java/org/secuso/privacyfriendlyfinance/domain/model/Category.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/domain/model/Category.java
@@ -1,4 +1,23 @@
 package org.secuso.privacyfriendlyfinance.domain.model;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "Category")
 public class Category {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
