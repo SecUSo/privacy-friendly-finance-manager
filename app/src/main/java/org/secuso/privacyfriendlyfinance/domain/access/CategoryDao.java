891a6d8d511f170f1a814d26b5e608319440a70f
Leo <leonard.otto@mailbox.org>
2018-11-14 14:17:17 +0100

    Added category and category dao. Trying to commit for the first time.

---
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/domain/access/CategoryDao.java
@@ -0,0 +1,4 @@
+package org.secuso.privacyfriendlyfinance.domain.access;
+
+public class CategoryDao {
+}
---
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/domain/model/Category.java
@@ -0,0 +1,4 @@
+package org.secuso.privacyfriendlyfinance.domain.model;
+
+public class Category {
+}
