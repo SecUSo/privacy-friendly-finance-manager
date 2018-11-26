e39f3a9b5b0db5ea7537ebe31a2f0c264170d331
Felix Hofmann <felix@hofmann.rocks>
2018-11-26 14:07:53 +0100

    added legacy database migration stub

---
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/domain/legacy/MigrationFromUnencrypted.java
@@ -0,0 +1,10 @@
+package org.secuso.privacyfriendlyfinance.domain.legacy;
+
+import org.secuso.privacyfriendlyfinance.domain.FinanceDatabase;
+
+public class MigrationFromUnencrypted {
+
+    public static void migrateTo(FinanceDatabase target) {
+        
+    }
+}
