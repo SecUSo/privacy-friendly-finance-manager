17910033cc1e89f27aa4e8d55d87775cbf852d5b
Felix Hofmann <felix@hofmann.rocks>
2018-12-14 23:36:23 +0100

    transaction dialog new UI with two-way-binding

--- app/src/main/java/org/secuso/privacyfriendlyfinance/activities/TransactionDialog.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/activities/TransactionDialog.java
@@ -21,38 +21,38 @@ import android.app.Dialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
+import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.joda.time.LocalDate;
+import org.secuso.privacyfriendlyfinance.BR;
 import org.secuso.privacyfriendlyfinance.R;
 import org.secuso.privacyfriendlyfinance.activities.adapter.NullableArrayAdapter;
 import org.secuso.privacyfriendlyfinance.activities.helper.CurrencyTextWatcher;
 import org.secuso.privacyfriendlyfinance.activities.viewmodel.TransactionDialogViewModel;
-import org.secuso.privacyfriendlyfinance.domain.convert.LocalDateConverter;
+import org.secuso.privacyfriendlyfinance.databinding.DialogTransactionBinding;
 import org.secuso.privacyfriendlyfinance.domain.model.Account;
 import org.secuso.privacyfriendlyfinance.domain.model.Category;
 import org.secuso.privacyfriendlyfinance.domain.model.Transaction;
 
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 /**
  * Dialog for adding new transactions and for editing existing transactions.
  *
- * @author Leonard Otto, Felix Hofmann
+ * @author Felix Hofmann, Leonard Otto
  */
 public class TransactionDialog extends AppCompatDialogFragment {
     public static final String EXTRA_CATEGORY_ID = "org.secuso.privacyfriendlyfinance.EXTRA_CATEGORY_ID";
@@ -61,41 +61,74 @@ public class TransactionDialog extends AppCompatDialogFragment {
 
     private AlertDialog dialog;
     private View view;
-    private EditText editTextTitle;
     private EditText editTextAmount;
     private TextView editTextDate;
-    private RadioButton radioButtonIncome;
-    private RadioButton radioButtonExpense;
-    private RadioGroup radioGroupType;
     private Spinner categorySpinner;
     private Spinner accountSpinner;
 
     private TransactionDialogViewModel viewModel;
-
-    private Transaction transactionObject;
-    private long preselectedAccountId = -1L;
-    private long preselectedCategoryId = -1L;
-
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         viewModel = ViewModelProviders.of(this).get(TransactionDialogViewModel.class);
-        viewModel.setTransactionId(getArguments().getLong(EXTRA_TRANSACTION_ID, -1L));
 
-        view = getActivity().getLayoutInflater().inflate(R.layout.dialog_transaction, null);
+        final DialogTransactionBinding binding = DataBindingUtil.inflate(LayoutInflater.from(getContext()), R.layout.dialog_transaction, null, false);
+        view = binding.getRoot();
         builder.setView(view);
 
-        editTextTitle = view.findViewById(R.id.dialog_transaction_title);
         editTextAmount = view.findViewById(R.id.dialog_transaction_amount);
         editTextDate = view.findViewById(R.id.dialog_transaction_date);
-        radioButtonIncome = view.findViewById(R.id.radioButton_transaction_income);
-        radioButtonExpense = view.findViewById(R.id.radioButton_transaction_expense);
-        radioGroupType = view.findViewById(R.id.radioGroup_transaction_type);
         categorySpinner = view.findViewById(R.id.category_spinner);
         accountSpinner = view.findViewById(R.id.account_spinner);
 
+        viewModel.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable sender, int propertyId) {
+                if (propertyId == BR.expense) {
+                    editTextAmount.setTextColor(getResources().getColor(viewModel.getAmountColor()));
+                }
+            }
+        });
+
+        long transactionId = getArguments().getLong(EXTRA_TRANSACTION_ID, -1L);
+
+        if (transactionId >= 0) {
+            builder.setTitle(R.string.dialog_transaction_edit_title);
+        } else {
+            builder.setTitle(R.string.dialog_transaction_create_title);
+        }
+        if (viewModel.getTransaction() == null) {
+            viewModel.setTransactionId(transactionId).observe(this, new Observer<Transaction>() {
+                @Override
+                public void onChanged(@Nullable Transaction transaction) {
+                    if (transaction.getId() == null) {
+                        Log.d("acc id", getArguments().getLong(EXTRA_ACCOUNT_ID, -1L) + "");
+                        transaction.setAccountId(getArguments().getLong(EXTRA_ACCOUNT_ID, -1L));
+                        transaction.setCategoryId(getArguments().getLong(EXTRA_CATEGORY_ID, -1L));
+                    }
+                    viewModel.setTransaction(transaction);
+                    binding.setViewModel(viewModel);
+                }
+            });
+        } else {
+            binding.setViewModel(viewModel);
+        }
+
+        viewModel.getAllAccounts().observe(this, new Observer<List<Account>>() {
+            @Override
+            public void onChanged(@Nullable List<Account> accounts) {
+                accountSpinner.setAdapter(new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, accounts));
+            }
+        });
+
+        viewModel.getAllCategories().observe(this, new Observer<List<Category>>() {
+            @Override
+            public void onChanged(@Nullable List<Category> categories) {
+                categorySpinner.setAdapter(new NullableArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, categories));
+            }
+        });
 
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
@@ -105,11 +138,11 @@ public class TransactionDialog extends AppCompatDialogFragment {
         builder.setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int which) {
-//                submitTransaction();
+                viewModel.submit();
             }
         });
 
-        editTextAmount.addTextChangedListener(new CurrencyTextWatcher(editTextAmount, "#,###"));
+        editTextAmount.addTextChangedListener(new CurrencyTextWatcher());
 
         editTextDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,158 +152,18 @@ public class TransactionDialog extends AppCompatDialogFragment {
         });
 
         dialog = builder.create();
-
-
-        viewModel.getTransaction().observe(this, new Observer<Transaction>() {
-            @Override
-            public void onChanged(@Nullable Transaction transaction) {
-                TransactionDialog.this.transactionObject = transaction;
-                if (transaction.getId() == null) {
-                    transaction.setAccountId(getArguments().getLong(EXTRA_ACCOUNT_ID, -1L));
-                    transaction.setCategoryId(getArguments().getLong(EXTRA_CATEGORY_ID, -1L));
-                    dialog.setTitle(R.string.dialog_transaction_create_title);
-                } else {
-                    dialog.setTitle(R.string.dialog_transaction_edit_title);
-                }
-
-                editTextTitle.setText(transaction.getName());
-                editTextDate.setText(transaction.getDateAsString());
-                setAmount(transaction.getAmount());
-            }
-        });
-
         return dialog;
     }
 
 
-
-    private void setAmount(long amount) {
-        editTextAmount.setText(String.valueOf((float) Math.abs(amount) / 100.0));
-        radioButtonExpense.setChecked(amount <= 0);
-        radioButtonIncome.setChecked(amount > 0);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-
-        viewModel.getAllAccounts().observe(this, new Observer<List<Account>>() {
-            @Override
-            public void onChanged(@Nullable List<Account> accounts) {
-                accountSpinner.setAdapter(new ArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, accounts));
-                if (preselectedAccountId != -1L) {
-                    for (int i = 0; i < accounts.size(); i++) {
-                        if (accounts.get(i).getId() == preselectedAccountId) {
-                            accountSpinner.setSelection(i);
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-
-        viewModel.getAllCategories().observe(this, new Observer<List<Category>>() {
-            @Override
-            public void onChanged(@Nullable List<Category> categories) {
-                List<Category> categoriesAndVoid = new ArrayList<>();
-                categoriesAndVoid.add(null);
-                categoriesAndVoid.addAll(categories);
-                categorySpinner.setAdapter(new NullableArrayAdapter<>(getActivity(), R.layout.support_simple_spinner_dropdown_item, categoriesAndVoid));
-                if (preselectedCategoryId != -1L) {
-                    for (int i = 0; i < categories.size(); i++) {
-                        if (categories.get(i).getId() == preselectedCategoryId) {
-                            categorySpinner.setSelection(i);
-                            break;
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-
-    private void setUpCreateDialog(AlertDialog.Builder builder) {
-        builder.setTitle(R.string.dialog_transaction_create_title);
-
-        //Fill the date field with the date 'now'
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH) + 1;
-        int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-
-        LocalDate ld = new LocalDate(year, month, dayOfMonth);
-        editTextDate.setText(LocalDateConverter.dateToString(ld));
-    }
-
-    private void dateSet(int year, int month, int dayOfMonth) {
-        LocalDate ld = new LocalDate(year, month + 1, dayOfMonth);
-
-        editTextDate.setText(LocalDateConverter.dateToString(ld));
-    }
-
     private void openDatePicker() {
-        //Open the date picker on the day that is already set
-        LocalDate ld = LocalDateConverter.fromString(editTextDate.getText().toString());
-        int year = ld.getYear();
-        int month = ld.getMonthOfYear() + 1;
-        int day = ld.getDayOfMonth();
-
-        DatePickerDialog dialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+        LocalDate date = viewModel.getDate();
+        new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                dateSet(year, month, dayOfMonth);
+                viewModel.setDate(new LocalDate(year, month + 1, dayOfMonth));
             }
-        }, year, month, day);
+        }, date.getYear(), date.getMonthOfYear() - 1, date.getDayOfMonth()).show();
         dialog.show();
-    }
-
-    private void submitTransaction() {
-        String tmpName = editTextTitle.getText().toString();
-
-        long tmpAmount = -1L;
-        if (editTextAmount.getText().toString() == null) {
-            //TODO: error message?
-            return;
-        } else {
-            try {
-                double tmpDoubleAmount = Double.parseDouble(editTextAmount.getText().toString());
-                tmpAmount = (long) (tmpDoubleAmount * 100.0);
-            } catch (NumberFormatException ex) {
-                //This line should not be reachable
-                //TODO: How to handle exceptions like that?
-                System.out.println("Error parsing transaction amount. Got string: " + editTextAmount.getText().toString());
-                return;
-            }
-        }
-
-        if (tmpAmount == 0) {
-            Toast toast = Toast.makeText(getContext(), getString(R.string.dialog_transaction_zero_value_impossible_msg), Toast.LENGTH_LONG);
-            toast.show();
-        } else {
-            boolean isExpense = false;
-            if (radioGroupType.getCheckedRadioButtonId() == R.id.radioButton_transaction_expense) {
-                isExpense = true;
-            } else {
-                isExpense = false;
-            }
-
-            String tmpDate = editTextDate.getText().toString();
-            Category tmpCategory = (Category) categorySpinner.getSelectedItem();
-            Account tmpAccount = (Account) accountSpinner.getSelectedItem();
-
-            transactionObject.setCategoryId(tmpCategory.getId());
-            transactionObject.setAccountId(tmpAccount.getId());
-            transactionObject.setAmount((isExpense ? -tmpAmount : tmpAmount));
-            transactionObject.setName(tmpName);
-            transactionObject.setDate(LocalDateConverter.fromString(tmpDate));
-
-            //Save the edited/created transaction
-            viewModel.editOrInsertTransaction(transactionObject);
-
-            Toast.makeText(getContext(), R.string.activity_transaction_saved_msg, Toast.LENGTH_SHORT).show();
-
-            dismiss();
-        }
     }
 }
--- app/src/main/java/org/secuso/privacyfriendlyfinance/activities/viewmodel/TransactionDialogViewModel.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/activities/viewmodel/TransactionDialogViewModel.java
@@ -1,11 +1,22 @@
 package org.secuso.privacyfriendlyfinance.activities.viewmodel;
 
 import android.app.Application;
+import android.arch.core.util.Function;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
+import android.databinding.Bindable;
+import android.databinding.Observable;
+import android.databinding.PropertyChangeRegistry;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
+import org.joda.time.LocalDate;
+import org.secuso.privacyfriendlyfinance.BR;
+import org.secuso.privacyfriendlyfinance.R;
+import org.secuso.privacyfriendlyfinance.activities.adapter.IdProvider;
+import org.secuso.privacyfriendlyfinance.activities.helper.CurrencyTextWatcher;
 import org.secuso.privacyfriendlyfinance.domain.FinanceDatabase;
 import org.secuso.privacyfriendlyfinance.domain.access.AccountDao;
 import org.secuso.privacyfriendlyfinance.domain.access.CategoryDao;
@@ -14,51 +25,225 @@ import org.secuso.privacyfriendlyfinance.domain.model.Account;
 import org.secuso.privacyfriendlyfinance.domain.model.Category;
 import org.secuso.privacyfriendlyfinance.domain.model.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class TransactionDialogViewModel extends AndroidViewModel {
+public class TransactionDialogViewModel extends AndroidViewModel implements Observable {
     private CategoryDao categoryDao = FinanceDatabase.getInstance().categoryDao();
     private AccountDao accountDao = FinanceDatabase.getInstance().accountDao();
     private TransactionDao transactionDao = FinanceDatabase.getInstance().transactionDao();
 
-    private LiveData<List<Category>> categories = categoryDao.getAll();
+//    private List<Category> categories;
+    private LiveData<List<Category>> categories;
     private LiveData<List<Account>> accounts = accountDao.getAll();
-    private LiveData<Transaction> transaction;
-    private MutableLiveData<Long> amount = new MutableLiveData<>();
+
+    private LiveData<Transaction> transactionLive;
+    private Transaction transaction;
 
 
     private long transactionId = -1;
 
     public TransactionDialogViewModel(@NonNull Application application) {
         super(application);
+        categories = Transformations.map(categoryDao.getAll(), new Function<List<Category>, List<Category>>() {
+            @Override
+            public List<Category> apply(List<Category> input) {
+                List<Category> categoriesAndVoid = new ArrayList<>();
+                categoriesAndVoid.add(null);
+                categoriesAndVoid.addAll(input);
+                return categoriesAndVoid;
+            }
+        });
+
+        Transformations.map(accounts, new Function<List<Account>, Void>() {
+            @Override
+            public Void apply(List<Account> input) {
+                notifyPropertyChanged(BR.accountIndex);
+                return null;
+            }
+        });
+
+        setTransactionDummy();
     }
 
     public LiveData<List<Category>> getAllCategories() {
         return categories;
     }
-
     public LiveData<List<Account>> getAllAccounts() {
         return accounts;
     }
 
-    public void editOrInsertTransaction(Transaction transaction) {
-        transactionDao.updateOrInsertAsync(transaction);
-    }
-
-    public void setTransactionId(long transactionId) {
+    public LiveData<Transaction> setTransactionId(long transactionId) {
         if (this.transactionId != transactionId) {
             this.transactionId = transactionId;
             if (transactionId == -1) {
-                MutableLiveData<Transaction> mutableTransaction = new MutableLiveData<>();
-                mutableTransaction.postValue(new Transaction());
-                transaction = mutableTransaction;
+                setTransactionDummy();
             } else {
-                transaction = transactionDao.get(transactionId);
+                transactionLive = transactionDao.get(transactionId);
             }
+        }
+        return transactionLive;
+    }
+
+    private void setTransactionDummy() {
+        MutableLiveData<Transaction> mutableTransaction = new MutableLiveData<>();
+        mutableTransaction.postValue(new Transaction());
+        transactionLive = mutableTransaction;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+        notifyChange();
+    }
+
+
+    @Bindable
+    public String getName() {
+        return transaction.getName();
+    }
+    public void setName(String name) {
+        if (name == null) name = "";
+        if (transaction.getName() == null) transaction.setName("");
+        if (!transaction.getName().equals(name)) {
+            transaction.setName(name);
+            notifyPropertyChanged(BR.name);
         }
     }
 
-    public LiveData<Transaction> getTransaction() {
-        return transaction;
+    @Bindable
+    public String getAmount() {
+        return CurrencyTextWatcher.convertToString(transaction.getAmount());
     }
+    public void setAmount(String amount) {
+        if (amount == null) amount = "";
+        Long number = CurrencyTextWatcher.convertToLong(amount);
+        if (number == null) number = 0L;
+        if (transaction.getAmount() != number){
+            transaction.setAmount(number);
+            notifyPropertyChanged(BR.amount);
+        }
+    }
+
+    @Bindable
+    public int getAmountColor() {
+        return getExpense() ? R.color.red : R.color.green;
+    }
+
+    @Bindable
+    public boolean getExpense() {
+        return transaction.getAmount() <= 0;
+    }
+    public void setExpense(boolean checked) {
+        if ((transaction.getAmount() > 0 && checked) || (transaction.getAmount() < 0 && !checked)) {
+            transaction.setAmount(transaction.getAmount() * -1);
+            Log.d("amount", "" + transaction.getAmount());
+            notifyPropertyChanged(BR.expense);
+            notifyPropertyChanged(BR.amountColor);
+        }
+    }
+
+    @Bindable
+    public String getDateString() {
+        return transaction.getDate().toString();
+    }
+    public LocalDate getDate() {
+        return transaction.getDate();
+    }
+    public void setDate(LocalDate date) {
+        if (date != null && !transaction.getDate().equals(date)){
+            transaction.setDate(date);
+            notifyPropertyChanged(BR.dateString);
+        }
+    }
+
+    private int indexOfId(List<? extends IdProvider> list, Long id) {
+        if (list == null) return 0;
+        for (int i = 0; i < list.size(); ++i) {
+            IdProvider element = list.get(i);
+            if ((element != null && element.getId() == id) || (element == null && id == null)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    @Bindable
+    public int getAccountIndex() {
+        if (accounts.getValue() == null) return 0;
+        return indexOfId(accounts.getValue(), transaction.getAccountId());
+    }
+    public void setAccountIndex(int accountIndex) {
+        Log.d("accountIndex", "" + accountIndex);
+        Account account = accounts.getValue().get(accountIndex);
+        if (transaction.getAccountId() != account.getId()) {
+            transaction.setAccountId(account.getId());
+            notifyPropertyChanged(BR.accountIndex);
+        }
+    }
+
+    @Bindable
+    public int getCategoryIndex() {
+        if (categories.getValue() == null) return 0;
+        return indexOfId(categories.getValue(), transaction.getCategoryId());
+    }
+    public void setCategoryIndex(int categoryIndex) {
+        Log.d("categoryIndex", "" + categoryIndex);
+        Category category = categories.getValue().get(categoryIndex);
+        if (category == null) {
+            if (transaction.getCategoryId() != null) {
+                transaction.setCategoryId(null);
+                notifyPropertyChanged(BR.categoryIndex);
+            }
+        } else if (transaction.getCategoryId() != category.getId()) {
+            transaction.setCategoryId(category.getId());
+            notifyPropertyChanged(BR.categoryIndex);
+        }
+    }
+
+
+    public void submit() {
+        transactionDao.updateOrInsertAsync(transaction);
+    }
+
+    // The following is just the BaseObservalbe implementation.
+    private transient PropertyChangeRegistry mCallbacks;
+    @Override
+    public synchronized void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        if (mCallbacks == null) {
+            mCallbacks = new PropertyChangeRegistry();
+        }
+        mCallbacks.add(callback);
+        Log.w("transDilg", "callback added");
+    }
+    @Override
+    public synchronized void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        if (mCallbacks != null) {
+            mCallbacks.remove(callback);
+        }
+    }
+    /**
+     * Notifies listeners that all properties of this instance have changed.
+     */
+    public synchronized void notifyChange() {
+        if (mCallbacks != null) {
+            mCallbacks.notifyCallbacks(this, 0, null);
+        }
+    }
+    /**
+     * Notifies listeners that a specific property has changed. The getter for the property
+     * that changes should be marked with {@link Bindable} to generate a field in
+     * <code>BR</code> to be used as <code>fieldId</code>.
+     *
+     * @param fieldId The generated BR id for the Bindable field.
+     */
+    public void notifyPropertyChanged(int fieldId) {
+        if (mCallbacks != null) {
+            mCallbacks.notifyCallbacks(this, fieldId, null);
+        }
+    }
+
+
 }
