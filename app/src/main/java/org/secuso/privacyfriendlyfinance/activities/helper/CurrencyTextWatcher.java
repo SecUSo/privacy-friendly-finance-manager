5d5fd3c4e267f2d5139722556d0d396421e7b132
Felix Hofmann <felix@hofmann.rocks>
2018-12-14 23:35:24 +0100

    CurrencyTextWatcher rewrite

--- app/src/main/java/org/secuso/privacyfriendlyfinance/activities/helper/CurrencyTextWatcher.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/activities/helper/CurrencyTextWatcher.java
@@ -2,83 +2,59 @@ package org.secuso.privacyfriendlyfinance.activities.helper;
 
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.widget.EditText;
+import android.util.Log;
 
 import java.text.DecimalFormat;
-import java.text.ParseException;
 
 public class CurrencyTextWatcher implements TextWatcher {
+    public static final DecimalFormat format = new DecimalFormat("#0.00");
+    boolean mEditing = false;
 
-    private final DecimalFormat df;
-    private final DecimalFormat dfnd;
-    private final EditText et;
-    private boolean hasFractionalPart;
-    private int trailingZeroCount;
-
-    public CurrencyTextWatcher(EditText editText, String pattern) {
-        df = new DecimalFormat(pattern);
-        df.setDecimalSeparatorAlwaysShown(true);
-        dfnd = new DecimalFormat("#,###.00");
-        this.et = editText;
-        hasFractionalPart = false;
+    public static String convertToString(Long value) {
+        if (value == null) return null;
+        return format.format(value.doubleValue() / 100.00);
+    }
+    public static String convertToString(Double value) {
+        if (value == null) return null;
+        return format.format(value);
+    }
+    private static String digitsOf(String input) {
+        return input.replaceAll("\\D", "");
     }
 
-    @Override
-    public void afterTextChanged(Editable s) {
-        et.removeTextChangedListener(this);
-
-        if (s != null && !s.toString().isEmpty()) {
-            try {
-                int inilen, endlen;
-                inilen = et.getText().length();
-                String v = s.toString().replace(String.valueOf(df.getDecimalFormatSymbols().getGroupingSeparator()), "").replace("$","");
-                Number n = df.parse(v);
-                int cp = et.getSelectionStart();
-                if (hasFractionalPart) {
-                    StringBuilder trailingZeros = new StringBuilder();
-                    while (trailingZeroCount-- > 0)
-                        trailingZeros.append('0');
-                    et.setText(df.format(n) + trailingZeros.toString());
-                } else {
-                    et.setText(dfnd.format(n));
-                }
-                et.setText("$".concat(et.getText().toString()));
-                endlen = et.getText().length();
-                int sel = (cp + (endlen - inilen));
-                if (sel > 0 && sel < et.getText().length()) {
-                    et.setSelection(sel);
-                } else if (trailingZeroCount > -1) {
-                    et.setSelection(et.getText().length() - 3);
-                } else {
-                    et.setSelection(et.getText().length());
-                }
-            } catch (NumberFormatException | ParseException e) {
-                e.printStackTrace();
-            }
-        }
-
-        et.addTextChangedListener(this);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        int index = s.toString().indexOf(String.valueOf(df.getDecimalFormatSymbols().getDecimalSeparator()));
-        trailingZeroCount = 0;
-        if (index > -1) {
-            for (index++; index < s.length(); index++) {
-                if (s.charAt(index) == '0')
-                    trailingZeroCount++;
-                else {
-                    trailingZeroCount = 0;
-                }
-            }
-            hasFractionalPart = true;
-        } else {
-            hasFractionalPart = false;
+    public static Long convertToLong(String text) {
+        try {
+            return Long.parseLong(digitsOf(text));
+        } catch (NumberFormatException e) {
+            return null;
         }
     }
+    public static Double convertToDouble(String text) {
+        try {
+            return Double.parseDouble(digitsOf(text)) / 100.0;
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public synchronized void afterTextChanged(Editable s) {
+        if (!mEditing) {
+            mEditing = true;
+            String out = convertToString(convertToLong(s.toString()));
+            Log.d("watcher", out);
+            if (out != null) {
+                s.replace(0, s.length(), out);
+            } else {
+                s.clear();
+            }
+            mEditing = false;
+        }
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
 }
