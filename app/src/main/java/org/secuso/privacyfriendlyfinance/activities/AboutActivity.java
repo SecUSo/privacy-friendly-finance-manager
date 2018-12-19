722f4da930a89947f27ef6c845a06009d69af0a8
Felix Hofmann <felix@hofmann.rocks>
2018-12-19 18:51:11 +0100

    cleaned up About Activity

--- app/src/main/java/org/secuso/privacyfriendlyfinance/activities/AboutActivity.java
+++ app/src/main/java/org/secuso/privacyfriendlyfinance/activities/AboutActivity.java
@@ -17,51 +17,21 @@
 
 package org.secuso.privacyfriendlyfinance.activities;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.LinkMovementMethod;
-import android.view.View;
 import android.widget.TextView;
 
-import org.secuso.privacyfriendlyfinance.R;
 import org.secuso.privacyfriendlyfinance.BuildConfig;
+import org.secuso.privacyfriendlyfinance.R;
 
-/**
- * @author David Meiborg
- * Activity that shows information of the app development.
- */
 public class AboutActivity extends AppCompatActivity {
-
-    @SuppressLint("StringFormatInvalid")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
-        ActionBar ab = getSupportActionBar();
-        if(ab != null) {
-            ab.setDisplayHomeAsUpEnabled(true);
-        }
-
-        View mainContent = findViewById(R.id.main_content);
-        if (mainContent != null) {
-            mainContent.setAlpha(0);
-            mainContent.animate().alpha(1).setDuration(BaseActivity.MAIN_CONTENT_FADEIN_DURATION);
-        }
-
-        overridePendingTransition(0, 0);
-
-        TextView tv =findViewById(R.id.textFieldVersion);
-        tv.setText(getString(R.string.activity_about_version, BuildConfig.VERSION_NAME));
-
-        ((TextView)findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
-        ((TextView)findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.textFieldVersion)).setText(getString(R.string.activity_about_version, BuildConfig.VERSION_NAME));
+        ((TextView) findViewById(R.id.secusoWebsite)).setMovementMethod(LinkMovementMethod.getInstance());
+        ((TextView) findViewById(R.id.githubURL)).setMovementMethod(LinkMovementMethod.getInstance());
     }
-
-    //@Override
-    //protected int getNavigationDrawerID() {
-    //    return R.id.nav_about;
-    //}
 }
 
